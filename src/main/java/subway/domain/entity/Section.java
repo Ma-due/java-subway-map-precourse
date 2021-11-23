3fc13ca03f7a8359d5213ba104b376d8cd80e0ca
Ma-due <maduee2@naver.com>
2021-11-23 21:49:36 +0900

    feat : 구간을 나타내는 Section 객체

---
+++ src/main/java/subway/domain/entity/Section.java
@@ -0,0 +1,36 @@
+package subway.domain.entity;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Section {
+    private String lineName;
+    private List<String> stations;
+
+    private Section(String lineName, String ascendingStation, String descendingStation) {
+        this.lineName = lineName;
+        this.stations = new LinkedList<>();
+        this.stations.add(ascendingStation);
+        this.stations.add(descendingStation);
+    }
+
+    public Section of(String lineName, String ascendingStation, String descendingStation) {
+        return new Section(lineName, ascendingStation, descendingStation);
+    }
+
+    public void addStation(String stationName, int index) {
+        this.stations.add(index, stationName);
+    }
+
+    public Section getSection() {
+        return this;
+    }
+
+    public String getLineName() {
+        return lineName;
+    }
+
+    public List<String> getStations() {
+        return stations;
+    }
+}
